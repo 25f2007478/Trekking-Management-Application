896f79fb7d5a2a2073a6d1641f2996df1f6c6692
Shashwatam Shashank <shashankshashwatam@gmail.com>
2026-07-03 21:23:32 +0530

    Milestone-3: Fully functioning Admin Dashboard added

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb.sqlite3'
 
 db.init_app(app)
 
-################# LOGIN & SIGNUP ###################
+#-------- LOGIN & SIGNUP --------#
 
 @app.route('/', methods=['GET','POST'])
 def user_login():
@@ -16,6 +16,7 @@ def user_login():
         email = request.form.get('email')
         password = request.form.get('password') 
         curr_user = User.query.filter_by(email=email).first()
+        
         if curr_user.role == 'Admin':
             if curr_user.password == password:
                 return redirect(url_for('admin_dashboard'))
@@ -123,14 +124,204 @@ def staff_apply():
         return render_template('onboarding.html', user='staff',msg='Application successful, Wait for Admin approval !')
     
 
+#------ADMIN FUNTIONALITIES------#
+
+@app.route("/admin_dashboard", methods=['GET','POST'])
+def admin_home():
+
+    staff_num = User.query.filter_by(role='staff', is_approved=True).count()
+    user_num = User.query.filter_by(role='trekker').count()
+    trek_num = Trek.query.filter_by(status='Open').count()
+
+    return render_template('admin_dashboard.html',func='home', total_staff=staff_num, total_users=user_num, active_treks=trek_num)
+
+@app.route('/admin_dashboard/staffs')                           
+def admin_staff():
+
+    active_staff= User.query.filter_by(role='staff', is_approved=True).all()
+    pending_staff= User.query.filter_by(role='staff', is_approved=False).all()
+    blacklisted_staff= User.query.filter_by(role='Blacklisted_staff').all()
+
+    return render_template('admin_dashboard.html',func='staff', pending_staffs=pending_staff, active_staffs=active_staff, blacklisted_staffs=blacklisted_staff)
+
+@app.route('/admin_dashboard/users')
+def admin_user():
+
+    active_user= User.query.filter_by(role='trekker', is_approved=True).all()
+    blacklisted= User.query.filter_by(role='Blacklisted').all()
+
+    return render_template('admin_dashboard.html',func='user', active_users=active_user, blacklisted=blacklisted)
+
+@app.route('/admin_dashboard/treks')
+def admin_trek():
+
+    active_treks = Trek.query.filter_by(status='Open').all()
+    completed_treks = Trek.query.filter_by(status='Completed').all()
+
+    return render_template('admin_dashboard.html', func='trek', active_treks=active_treks, completed_treks=completed_treks)
+
+@app.route('/approve/<int:staff_id>', methods=['POST'])
+def approve(staff_id):
+
+    staff = User.query.get(staff_id)
+    staff.is_approved = True
+
+    speciality=request.form.get('speciality')
+
+    staff_prof = StaffProfile(
+        user_id=staff_id,
+        speciality=speciality
+    )
+
+    db.session.add(staff_prof)
+    db.session.commit()
+
+    return redirect(url_for('admin_staff'))
+
+@app.route('/reject/<int:staff_id>', methods=['POST'])
+def fire_staff(staff_id):
+    staff = User.query.get_or_404(staff_id)
+    target = ''
+
+    if staff.staff_profile:
+        target = 'admin_staff'
+        db.session.delete(staff.staff_profile)
+    else:
+        target = 'admin_user'
+    
+    db.session.delete(staff)
+    db.session.commit()
+    return redirect(url_for(target))
 
 
+@app.route('/blacklist/<int:user_id>', methods=['POST'])
+def blacklist(user_id):
 
+    user = User.query.get(user_id)
 
+    user.is_approved = False
+    target = ''
 
+    if user.staff_profile:
+        user.staff_profile.status = 'Blacklisted'
+        user.role = 'Blacklisted_staff'
+        target = 'admin_staff'
+    
+    else:
+        user.role = 'Blacklisted'
+        target = 'admin_user'
 
+    db.session.commit()
 
+    return redirect(url_for(target))
 
+@app.route('/unblacklist/<int:user_id>', methods=['POST'])
+def unblacklist(user_id):
+
+    user = User.query.get(user_id)
+    target = ''
+    user.is_approved = True
+
+    if user.staff_profile:
+        user.staff_profile.status = 'Available'
+        user.role = 'staff'
+        target = 'admin_staff'
+    
+    else:
+        user.role = 'trekker'
+        target = 'admin_user'
+        
+    db.session.commit()
+
+    return redirect(url_for(target))
+
+@app.route('/addtrek', methods=['GET','POST'])
+def add_trek():
+
+    if request.method == 'GET':
+
+        active_staff= User.query.filter_by(role='staff', is_approved=True).all()
+        available_staff = []
+        for staff in active_staff:
+            if staff.staff_profile.status == 'Available':
+                available_staff.append(staff)
+
+        return render_template('trek_add.html', available_staffs=available_staff)
+    
+    else:
+        name = request.form.get('name')
+        location = request.form.get('location')
+        difficulty = request.form.get('difficulty')
+        duration = request.form.get('duration')
+        available_slots = request.form.get('available_slots')
+        img = request.form.get('img')
+        assigned_staff_id = request.form.get('assigned_staff')
+        print(assigned_staff_id)
+        new_trek = Trek(
+            trek_name = name,
+            location = location,
+            difficulty = difficulty,
+            duration = duration,
+            available_slots = available_slots,
+            img = img,
+            assigned_staff_id = assigned_staff_id
+        )
+
+        staff = StaffProfile.query.get(assigned_staff_id)
+        staff.status = 'Occupied'
+
+        db.session.add(new_trek)
+        db.session.commit()
+
+        return redirect(url_for('admin_trek'))
+
+@app.route('/edittrek/<int:trek_id>', methods=['GET','POST'])
+def edit_trek(trek_id):
+
+   curr_trek = Trek.query.filter_by(id=trek_id).first()
+
+   if request.method == 'GET':
+        
+        active_staff= User.query.filter_by(role='staff', is_approved=True).all()
+        available_staff = []
+        for staff in active_staff:
+            if staff.staff_profile.status == 'Available':
+                available_staff.append(staff)
+
+        return render_template('trek_edit.html', trek=curr_trek, available_staffs=available_staff)
+   
+   else:
+    
+        curr_trek.name = request.form.get('name')
+        curr_trek.location = request.form.get('location')
+        curr_trek.difficulty = request.form.get('difficulty')
+        curr_trek.duration = request.form.get('duration')
+        curr_trek.available_slots = request.form.get('available_slots')
+        curr_trek.img = request.form.get('img')
+        curr_trek.staff_profile.status = 'Available'
+        new_staff_id = request.form.get('assigned_staff')
+        curr_trek.assigned_staff_id = new_staff_id
+        new_staff = StaffProfile.query.get(new_staff_id)
+        if new_staff:
+            new_staff.status = 'Occupied'
+
+        db.session.commit()
+        return redirect(url_for('admin_trek'))
+    
+@app.route('/deletetrek/<int:trek_id>', methods=['POST'])
+def delete_trek(trek_id):
+
+    curr_trek = Trek.query.filter_by(id=trek_id).first()
+
+    staff_id = curr_trek.assigned_staff_id 
+    curr_staff = StaffProfile.query.get(staff_id)
+    if curr_staff:
+        curr_staff.status = 'Available'
+
+    db.session.delete(curr_trek)
+    db.session.commit()
+
+    return redirect(url_for('admin_trek'))
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -21,31 +21,31 @@ class User(db.Model):
     staff_profile = db.relationship('StaffProfile', backref='user', uselist=False)
     # 2. User <-> Booking (One-to-Many)
     bookings = db.relationship('Booking', backref='user')
-    # 3. Trek Staff <-> Trek (One-to-Many)
-    assigned_treks = db.relationship('Trek', backref='user')
 
 class StaffProfile(db.Model):
     __tablename__ = 'staff_profiles'
     
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False, unique=True)
-    
-    specialty = db.Column(db.String(50), nullable=True) 
-    experience_years = db.Column(db.Integer, nullable=True)
+    speciality = db.Column(db.String(50), nullable=True) 
+    status = db.Column(db.String(20), default='Available') 
 
     # --- RELATIONSHIPS --- 
     # Trek Staff <-> Trek (One-to-Many)
     assigned_treks = db.relationship('Trek', backref='staff_profile')
+
     
 class Trek(db.Model):
     __tablename__ = 'treks'
     
     id = db.Column(db.Integer, primary_key=True)
     trek_name = db.Column(db.String(100), nullable=False)
+    location = db.Column(db.String(50), nullable=False)
     difficulty = db.Column(db.String(20), nullable=False) 
     duration = db.Column(db.Integer, nullable=False)
     available_slots = db.Column(db.Integer, nullable=False)
-    status = db.Column(db.String(20), default='Pending') 
+    status = db.Column(db.String(20), default='Open') 
+    img = db.Column(db.String(100), unique=True, nullable=False)
     assigned_staff_id = db.Column(db.Integer, db.ForeignKey('staff_profiles.id'), nullable=True)
     
     # --- RELATIONSHIPS ---
@@ -62,3 +62,5 @@ class Booking(db.Model):
     booking_status = db.Column(db.String(20), default='Confirmed') 
     payment_status = db.Column(db.String(20), default='Pending') 
     booking_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    
